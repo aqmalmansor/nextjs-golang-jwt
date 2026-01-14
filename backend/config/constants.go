1aabd152e94d9fbc379e6157ec6a74bc043b9b86
aqmalmansor <kifasu@gmail.com>
2026-01-14 18:43:57 +0800

    enhanced: readme and env

--- backend/config/constants.go
+++ backend/config/constants.go
@@ -7,12 +7,6 @@ import (
 
 var (
 	CLIENT_URL             = utils.GetEnvString("CLIENT_URL", "http://localhost:3000")
-	DB_PATH                = utils.GetEnvString("DB_PATH", "./database.db")
-	DB_HOST                = utils.GetEnvString("DB_HOST", "localhost")
-	DB_PORT                = utils.GetEnvNumber("DB_PORT", 3306)
-	DB_USER                = utils.GetEnvString("DB_USER", "root")
-	DB_PASSWORD            = utils.GetEnvString("DB_PASSWORD", "your_password")
-	DB_NAME                = utils.GetEnvString("DB_NAME", "ecommerce")
 	PORT                   = utils.GetEnvNumber("PORT", 8080)
 	JWT_SECRET             = utils.GetEnvString("JWT_SECRET", "your-secret")
 	JWT_CLAIMS_KEY_USER_ID = "userID"
